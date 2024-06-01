216ccb7315b6f6451ec0e6cd786d2c84e47b4949
tien <tien.2nghin@proton.me>
2024-06-01 18:30:54 +0700

    feat: display new quote for all when quote saved to db

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,11 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit lambda {
+                        broadcast_prepend_to 'quotes',
+                                             partial: 'quotes/quote',
+                                             locals: { quote: self },
+                                             target: 'quotes'
+                      }
 end
