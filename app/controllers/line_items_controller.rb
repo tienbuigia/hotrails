21f4df658c19044e39911934242336c28c704ac8
tien <tien.2nghin@proton.me>
2024-06-02 15:48:35 +0700

    feat: edit,update,destroy LineItem

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   before_action :set_quote
   before_action :set_line_item_date
+  before_action :set_line_item, only: %i[edit update destroy]
 
   def new
     @line_item = @line_item_date.line_items.build
@@ -16,6 +17,22 @@ class LineItemsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @line_item.update(line_item_params)
+      redirect_to @quote, notice: 'Item was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @line_item.destroy
+
+    redirect_to @quote, notice: 'Item was successfully destroyed.'
+  end
+
   private
 
   def line_item_params
@@ -28,5 +45,9 @@ class LineItemsController < ApplicationController
 
   def set_line_item_date
     @line_item_date = @quote.line_item_dates.find_by(id: params[:line_item_date_id])
+  end
+
+  def set_line_item
+    @line_item = @line_item_date.line_items.find_by(id: params[:id])
   end
 end
