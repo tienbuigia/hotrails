4cd9559fd32a36db60d67dd5ddb1ed4cf30c2dc5
tien <tien.2nghin@proton.me>
2024-06-02 17:56:26 +0700

    fix: wrong test value

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class QuoteTest < ActiveSupport::TestCase
   test '#total_price returns the sum of the total price of all line items' do
-    assert_equal 250, quotes(:first).total_price
+    assert_equal 2500, quotes(:first).total_price
   end
 end
